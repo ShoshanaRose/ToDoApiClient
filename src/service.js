6e98d44f086c3a0bac16cdcb10b24796ed4b3aa7
יהודית מלול <yuma8153@gmail.com>
2025-03-04 11:52:34 +0200

    Initial commit

--- src/service.js
+++ src/service.js
@@ -1,26 +1,45 @@
 import axios from 'axios';
 
-const apiUrl = "https://localhost:7271"
+// הגדרת כתובת ה-API כ-default
+axios.defaults.baseURL = "http://localhost:5017";//const apiUrl = "http://localhost:5017"
+
+// הגדרת interceptors
+axios.interceptors.response.use(
+  response => response, // מחזיר את התגובה אם אין שגיאה
+  error => {
+    console.error('Error response:', error); // רושם את השגיאה ללוג
+    return Promise.reject(error); // מחזיר את השגיאה
+  }
+);
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)    
+    const result = await axios.get(`/items`)    
     return result.data;
   },
 
-  addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
-    return {};
+  addTask: async(todo)=>{
+    console.log('addTask', todo)
+    const res = await axios.post(`/item?todo=${todo}`)
+    return res.data;
   },
 
   setCompleted: async(id, isComplete)=>{
     console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+    const res = await axios.put(`/item/${id}?isComplete=${isComplete}`)
+    return res.data;
   },
 
-  deleteTask:async()=>{
+  deleteTask:async(id)=>{
     console.log('deleteTask')
+    const res = await axios.delete(`/item/${id}`)
+    return res.data;
   }
 };
+
+// אפליקציית הקליינט משתמשת ב-axios כדי להוציא קריאות http.
+
+// הוסיפי את השדרוגים הבאים:
+
+// הגדירי את כתובת ה-api כ-default. השתמשי ב-Config Defaults.
+// הוסיפי interceptor שתופס את השגיאות ב-response ורושם ללוג.
